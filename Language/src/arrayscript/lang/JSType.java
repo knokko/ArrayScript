a941b7df7fc0f4d1e8e91009ada036492c65ddd3
knokko <knokogator@hotmail.com>
2019-01-28 20:34:33 +0100

    Initial commit

---
+++ Language/src/arrayscript/lang/JSType.java
@@ -0,0 +1,16 @@
+package arrayscript.lang;
+
+/**
+ * The basic types of plain javascript. A large array will be created to store variables and fields for
+ * each javascript type.
+ * @author knokko
+ *
+ */
+public enum JSType {
+	
+	BOOLEAN,
+	NUMBER,
+	STRING,
+	ARRAY,
+	OBJECT;
+}
