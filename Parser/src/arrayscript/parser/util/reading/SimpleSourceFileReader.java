e8ec0ba71bafc6e653b90fdcbe868893c3858b2c
knokko <knokogator@hotmail.com>
2019-01-30 22:27:45 +0100

    Improve reader such that it does not interrupt strings

--- Parser/src/arrayscript/parser/util/reading/SimpleSourceFileReader.java
+++ Parser/src/arrayscript/parser/util/reading/SimpleSourceFileReader.java
@@ -4,15 +4,18 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class SimpleSourceFileReader implements SourceFileReader {
-	
+
 	private static boolean isSeparator(char value) {
 		return Character.isWhitespace(value);
 	}
-	
+
 	private Scanner scanner;
 	private String currentLine;
 	private int currentIndex;
-	
+
+	private boolean inDoubleString;
+	private boolean inSingleString;
+
 	public SimpleSourceFileReader(Scanner fileScanner) {
 		scanner = fileScanner;
 	}
@@ -25,16 +28,23 @@ public class SimpleSourceFileReader implements SourceFileReader {
 
 		if (currentIndex >= currentLine.length()) {
 
-			// End of line has been reached, so try to read the next line in the next if block
+			// End of line has been reached, so try to read the next line in the next if
+			// block
 			currentLine = null;
 		}
 	}
-	
-	private void skipCurrentFileWhitespaces() {
-		if (currentLine != null) {
+
+	private void skipCurrentFileWhitespaces() throws IOException {
+		if (currentLine != null && !inDoubleString && !inSingleString) {
 			skipCurrentLineWhitespaces();
 		}
 		while (scanner != null && currentLine == null) {
+
+			// All strings must be terminated before the end of its line
+			if (inDoubleString || inSingleString) {
+				throw new IOException("Unterminated string");
+			}
+
 			if (scanner.hasNextLine()) {
 
 				// Start at the new line
@@ -43,7 +53,7 @@ public class SimpleSourceFileReader implements SourceFileReader {
 				skipCurrentLineWhitespaces();
 			} else {
 
-				// End of file has been reached, so try to read the next file in the next if block
+				// End of file has been reached
 				scanner.close();
 				currentLine = null;
 				return;
@@ -54,18 +64,35 @@ public class SimpleSourceFileReader implements SourceFileReader {
 	@Override
 	public String next() throws IOException {
 		skipCurrentFileWhitespaces();
-		
+
 		if (currentLine == null) {
-			
+
 			// If we reach this, the end of the file has been reached
 			return null;
 		}
 
 		// By now, the character at the currentIndex should be non-whitespace
-		int endIndex = currentIndex + 1;
-		while (endIndex < currentLine.length() && !isSeparator(currentLine.charAt(endIndex))) {
+		int endIndex = currentIndex;
+		char current = endIndex < currentLine.length() ? currentLine.charAt(endIndex) : 0;
+		while (endIndex < currentLine.length() && (!isSeparator(current) || inDoubleString || inSingleString)) {
+			//System.out.println("current char is " + current);
+			if (current == '"' && !inSingleString) {
+				System.out.println("swap in double string");
+				inDoubleString = !inDoubleString;
+			} else if (current == '\'' && !inDoubleString) {
+				inSingleString = !inSingleString;
+			}
 			endIndex++;
+			//System.out.println("change endIndex to " + endIndex);
+			if (endIndex < currentLine.length()) {
+				current = currentLine.charAt(endIndex);
+				//System.out.println("change current to " + current);
+			}
 		}
+		if (endIndex == currentLine.length() && (inSingleString || inDoubleString)) {
+			throw new IOException("Unterminated string");
+		}
+		System.out.println("currentIndex is " + currentIndex + " and endIndex is " + endIndex);
 		String result = currentLine.substring(currentIndex, endIndex);
 		currentIndex = endIndex + 1;
 		return result;
--- Parser/src/arrayscript/parser/util/reading/SourceFileReader.java
+++ Parser/src/arrayscript/parser/util/reading/SourceFileReader.java
@@ -12,7 +12,9 @@ public interface SourceFileReader {
 	
 	/**
 	 * Reads the next String that is separated by whitespace characters or line endings.
-	 * If the end of the current line is reached, it will go on with the next line. If the end of the
+	 * If the end of the current line is reached, it will go on with the next line. As an exception, this
+	 * method will not interrupt strings. So if there is a whitespace character within a string, that
+	 * whitespace character will be included in the result and the reading will continue. If the end of the
 	 * current source file is reached, it will return null.
 	 * @return The next String in this source file or null if the end of the file has been reached
 	 */
